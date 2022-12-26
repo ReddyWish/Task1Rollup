a464b94b1f440e9170ff55708a0672bfca22baf2
Ilya <ilya.shuvatov1@gmail.com>
2022-12-26 13:42:16 +0100

    added some plugins

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+import "./index.css";
+import jsImg from './assets/Unofficial_JavaScript_logo_2.svg.png';
+
+
 console.log('Hello World!')
+
+const jsImageJs = document.createElement('img')
+
+jsImageJs.className = 'js-image'
+
+jsImageJs.src = jsImg
+
+document.body.append(jsImageJs)
--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,8 @@
+import { babel } from '@rollup/plugin-babel';
+import styles from "rollup-plugin-styles";
+import image from 'rollup-plugin-img';
+import serve from 'rollup-plugin-serve'
+
 export default {
     input: './index.js',
     output: {
@@ -6,5 +11,17 @@ export default {
     },
     watch: {
         exclude: 'node_modules/**'
-    }
+    },
+    plugins: [
+        babel({ babelHelpers: 'bundled' }),
+        styles(),
+        image({
+            limit: 10000
+        }),
+        serve({
+            open:  true,
+            contentBase: './',
+            port: 8000,
+        })
+    ]
 };
